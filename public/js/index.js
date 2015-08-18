1863de2ea222ddb9196eee6bb4550f3bc79d38c1
Sonia Brahmi <sonia.brahmi@lightspeedpos.com>
2015-08-17 21:27:19 -0400

    Fixed filtering functionality
    - Fixed the ablity to filter down products
      based on checkboxes selection.

--- public/js/index.js
+++ public/js/index.js
@@ -1,175 +1,231 @@
-$(function() {
-	var products = [],
-		filters = {};
+'use strict';
+/* globals $:false */
+var products = [],
+	filters = {};
 
-	function Product() {
-		this.$checkboxes = $('.all-products input[type=checkbox]');
-		this.$singleProductPage = $('.single-product');
-		this.$window = $(window);
+/**
+ * @class Product
+ * @constructor
+ */
+var Product = function() {
+	// jQuery selectors.
+	this.$checkboxes = $('.all-products input[type=checkbox]');
+	this.$window = $(window);
+	this.$homePageContent = $('.main-content .page');
+	this.$clearFilterButton = $('.filters button');
 
-		this.$checkboxes.on('click',  this._checkboxesClick.bind(this));
-		this.$singleProductPage.on('click',  this._singleProductPageClick.bind(this));
-		this.$window.on('hashchange', this._windowHashChange.bind(this));
+	//Handlbar templates.
+	this.$productCellTemplate = $('#products-template');
 
-		$.getJSON('product.json', function(data) {
-			this.generateAllProductsHTML(data);
-			this.$window.trigger('hashchange');
-		}.bind(this));
-	}
+	// Pages.
+	this.$errorPage = $('.error');
+	this.$singleProductPage = $('.single-product');
+	this.$allProductsPage = $('.all-products');
 
-	Product.prototype._checkboxesClick = function(e) {
-		e.preventDefault();
-		var that = $(this),
-			specName = that.attr('name');
+	this.bindEvents();
 
-		if(that.is(':checked')) {
-			if(!(filters[specName] && filters[specName].length)){
-				filters[specName] = [];
-			}
+	$.getJSON('product.json', function(data) {
+		products = data;
+		this.generateAllProductsHTML(data);
+		this.$window.trigger('hashchange');
+	}.bind(this));
+};
 
-			//	Push values into the chosen filter array
-			filters[specName].push(that.val());
+/**
+ * Bind events
+ */
+Product.prototype.bindEvents = function() {
+	this.$checkboxes.on('click', this._checkboxesClick.bind(this));
+	this.$singleProductPage.on('click',  this._singleProductPageClick.bind(this));
+	this.$window.on('hashchange', this._windowHashChange.bind(this));
+};
 
-			// Change the url hash;
-			createQueryHash(filters);
+/**
+ *
+ * @param e jQuery event.
+ * @private
+ */
+Product.prototype._checkboxesClick = function(e) {
+	var checkbox = $(e.target),
+		specName = checkbox.attr('name');
+
+	if(checkbox.is(':checked')) {
+		if(!(filters[specName] && filters[specName].length)){
+			filters[specName] = [];
 		}
 
-		if(that.is(':checked') === false) {
-			if(filters[specName] && filters[specName].length && (filters[specName].indexOf(that.val()) != -1)){
-				var index = filters[specName].indexOf(that.val());
+		//	Push values into the chosen filter array
+		filters[specName].push(checkbox.val());
 
-				filters[specName].splice(index, 1);
+		// Change the url hash;
+		this.createQueryHash(filters);
+	}
 
-				if(!filters[specName].length){
-					delete filters[specName];
-				}
+	if(checkbox.is(':checked') === false) {
+		if(filters[specName] &&
+			filters[specName].length &&
+			(filters[specName].indexOf(checkbox.val()) != -1)) {
+			var index = filters[specName].indexOf(checkbox.val());
 
+			filters[specName].splice(index, 1);
+
+			if(filters[specName].length === 0){
+				delete filters[specName];
 			}
-			// Change the url hash;
+		}
+
+		// Change the url hash;
+		this.createQueryHash(filters);
+	}
+
+	this.$clearFilterButton.click(function (e) {
+		e.preventDefault();
+		window.location.hash = '#';
+	});
+};
+
+/**
+ *
+ * @param e jQuery event
+ * @private
+ */
+Product.prototype._singleProductPageClick = function(e) {
+
+	if (this.$singleProductPage.hasClass('visible')) {
+		var clicked = $(e.target);
+
+		// If the close button or the background are clicked go to the previous page.
+		if (clicked.hasClass('close') || clicked.hasClass('overlay')) {
+			// Change the url hash with the last used filters.
 			this.createQueryHash(filters);
 		}
+	}
+};
 
-		$('.filters button').click(function (e) {
-			e.preventDefault();
-			window.location.hash = '#';
-		});
-	};
+Product.prototype._windowHashChange = function() {
+	this.render(window.location.hash);
+};
 
-	Product.prototype._singleProductPageClick = function(e) {
+/**
+ *
+ * @param url
+ */
+Product.prototype.render = function(url) {
+	var temp = url.split('/')[0];
+	this.$homePageContent.removeClass('visible');
 
-		if (this.$singleProductPage.hasClass('visible')) {
-			var clicked = $(e.target);
+	var map = {
+		// Homepage
+		'': function() {
+			filters = {};
+			this.$checkboxes.prop('checked', false);
+			this.renderProductsPage(products);
+		}.bind(this),
+		// Single Product page
+		'#product': function() {
+			var index = url.split('#product/')[1].trim();
+			this.renderSingleProductPage(index, products);
+		}.bind(this),
+		// Page with filtered products
+		'#filter': function() {
+			url = url.split('#filter/')[1].trim();
 
-			// If the close button or the background are clicked go to the previous page.
-			if (clicked.hasClass('close') || clicked.hasClass('overlay')) {
-				// Change the url hash with the last used filters.
-				this.createQueryHash(filters);
+			try {
+				filters = JSON.parse(url);
 			}
-		}
-	};
-
-	Product.prototype._windowHashChange = function() {
-		this.render(window.location.hash);
-	};
-
-	Product.prototype.render = function(url) {
-		var temp = url.split('/')[0];
-		$('.main-content .page').removeClass('visible');
-
-		var map = {
-			// Homepage
-			'': function() {
-				filters = {};
-				this.$checkboxes.prop('checked', false);
-				this.renderProductsPage(products);
-			}.bind(this),
-			// Single Product page
-			'#product': function() {
-				var index = url.split('#product/')[1].trim();
-				this.renderSingleProductPage(index, products);
-
-			}.bind(this),
-			// Page with filtered products
-			'#filter': function() {
-				url = url.split('#filter/')[1].trim();
-
-				try {
-					filter = JSON.parse(url);
-				}
-				catch(err) {
-					window.location.hash = '#';
-				}
-
-				this.renderFilterResults(filters, products);
+			catch(err) {
+				window.location.hash = '#';
 			}
-		};
 
-		if(map[temp]) {
-			map[temp]();
-		}
-		else {
-			this.renderErrorPage();
-		}
+			this.renderFilterResults(filters, products);
+		}.bind(this)
+	};
+
+	if(map[temp]) {
+		map[temp]();
 	}
-
-	Product.prototype.generateAllProductsHTML = function(data) {
-		var list = $('.all-products .products-list');
-
-		var templateScript = $('#products-template').html();
-		var template = Handlebars.compile(templateScript);
-		list.append(template(data));
-
-		list.find('li').on('click', function(e) {
-			e.preventDefault();
-			var productIndex = $(this).data('index');
-			window.location.hash = 'product/' + productIndex;
-		})
+	else {
+		this.renderErrorPage();
 	}
+};
 
-	Product.prototype.renderProductsPage = function(data) {
-		var page = $('.all-products'),
-			allProducts = $('.all-products .products-list > li');
+/**
+ *
+ * @param data
+ */
+Product.prototype.generateAllProductsHTML = function(data) {
+	var list = $('.all-products .products-list');
 
-//		allProducts.addClass('hidden');
+	var templateScript = this.$productCellTemplate.html();
+	var template = Handlebars.compile(templateScript);
+	list.append(template(data));
 
-		allProducts.each(function() {
-			var that = $(this);
+	list.find('li').on('click', function(e) {
+		e.preventDefault();
+		var productIndex = $(this).data('index');
+		window.location.hash = 'product/' + productIndex;
+	}.bind(this))
+};
 
-			data.forEach(function(item) {
-				if(that.data('index') == item.id) {
-					that.removeClass('hidden');
-				}
-			});
+/**
+ *
+ * @param data
+ */
+Product.prototype.renderProductsPage = function(data) {
+	var page = this.$allProductsPage,
+		allProducts = $('.all-products .products-list > li');
+
+	allProducts.addClass('hidden');
+
+	allProducts.each(function() {
+		var that = $(this);
+
+		data.forEach(function(item) {
+			if(that.data('index') == item.id) {
+				that.removeClass('hidden');
+			}
 		});
+	});
 
-		page.addClass('visible');
+	page.addClass('visible');
+};
+
+/**
+ *
+ * @param index
+ * @param data
+ */
+Product.prototype.renderSingleProductPage = function(index, data) {
+	var page = this.$singleProductPage,
+		container = $('.preview-large');
+
+	if(data.length) {
+		data.forEach(function(item) {
+			if(item.id == index) {
+				container.find('h3').text(item.name);
+				container.find('img').attr('src', item.image.large);
+				container.find('p').text(item.description);
+			}
+		});
 	}
 
-	Product.prototype.renderSingleProductPage = function(index, data) {
-		var page = $('.single-product'),
-			container = $('.preview-large');
+	page.addClass('visible');
+};
 
-		if(data.length) {
-			data.forEach(function(item) {
-				if(item.id == index) {
-					container.find('h3').text(item.name);
-					container.find('img').attr('src', item.image.large);
-					container.find('p').text(item.description);
-				}
-			});
-		}
+/**
+ *
+ * @param filters
+ * @param products
+ */
+Product.prototype.renderFilterResults = function(filters, products) {
+	var criteria = ['manufacturer', 'style', 'color', 'heel'],
+		results = [],
+		isFiltered = false;
 
-		page.addClass('visible');
-	}
+	this.$checkboxes.prop('checked', false);
 
-	Product.prototype.renderFilterResults = function(filters, products) {
-		var criteria = ['manufacturer', 'style', 'color', 'heel'],
-			results = [],
-			isFiltered = false;
-
-		this.$checkboxes.prop('checked', false);
-
-		criteria.forEach(function(criterion) {
+	criteria.forEach(
+		function(criterion) {
 			if(filters[criterion] && filters[criterion].length) {
 				if(isFiltered) {
 					products = results;
@@ -198,23 +254,31 @@ $(function() {
 					}
 				});
 			}
-		});
-		this.renderProductsPage(results);
-	}
-
-	Product.prototype.renderErrorPage = function() {
-		var page = $('.error');
-		page.addClass('visible');
-	}
-
-	Product.prototype.createQueryHash = function(filters) {
-		if($.isEmptyObject(filters) === false) {
-			window.location.hash = '#filter/' + JSON.stringify(filters);
 		}
-		else {
-			window.location.hash = '#';
-		}
-	}
+	);
 
-	var product = new Product();
-});
+	this.renderProductsPage(results);
+};
+
+/**
+ *
+ */
+Product.prototype.renderErrorPage = function() {
+	var page = this.$errorPage;
+	page.addClass('visible');
+};
+
+/**
+ *
+ * @param filters
+ */
+Product.prototype.createQueryHash = function(filters) {
+	if($.isEmptyObject(filters) === false) {
+		window.location.hash = '#filter/' + JSON.stringify(filters);
+	}
+	else {
+		window.location.hash = '#';
+	}
+};
+
+var product = new Product();
